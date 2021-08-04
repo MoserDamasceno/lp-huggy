6f3fef078b1ca4c5987ee496a400a715a5162eaa
Moser Damasceno <moser.damasceno@gmail.com>
2021-08-04 13:19:26 -0300

    finish animation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,3 +12,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 });
+
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+  };
+  
+  const Obs = new IntersectionObserver(inViewport);
+  const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+  
+  // Attach observer to every [data-inviewport] element:
+  const ELs_inViewport = document.querySelectorAll('section');
+  ELs_inViewport.forEach(EL => {
+    Obs.observe(EL, obsOptions);
+  });
